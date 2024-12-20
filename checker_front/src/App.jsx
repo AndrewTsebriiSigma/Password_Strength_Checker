258a836264d29e321e3cb3626d32d07861c7c692
Andrii Tsebrii <andriitsebrii@gmail.com>
2024-12-20 18:36:01 -0500

    Implementing first styles
    Creating first html element and style them.

--- checker_front/src/App.jsx
+++ checker_front/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header.jsx'
-import Main from './components/Main.sx'
+import Main from './components/Main.jsx'
 
 function App() {
 
--- checker_front/src/components/Header.jsx
+++ checker_front/src/components/Header.jsx
@@ -1,5 +1,31 @@
-function Header() {
+import { useState } from "react"
+import '../App.css'
 
+function Header() {
+    const [response, setResponse] = useState('')
+
+    function handleResponse() {
+        setResponse("Strong") // retrieve response from model (either strong or weak)
+    }
+
+    return (
+        <>
+            <div className="description">
+                <h1>How Secure is Your Password?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Dignissimos iste ducimus architecto nam illum maiores fuga iure porro, quaerat, repellat, quidem provident. 
+                    Quis, tenetur. Ut id doloremque et consectetur neque.
+                </p>
+
+                <div className="inputField">
+                    <label htmlFor="">Enter your password:</label>
+                    <input type="text"/>
+                    <button onClick={handleResponse}>Check</button>
+                    <p>{response}</p>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Header;
